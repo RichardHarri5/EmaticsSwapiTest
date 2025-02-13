4fbcac9d4d745ef22b38daab49d22a56acdda10f
Richard-A-Harris <richard.harris@timpson.com>
2025-02-13 20:21:36 +0000

    GetFilms request added

--- EmaticsSwapiTest/Models/Film.cs
+++ EmaticsSwapiTest/Models/Film.cs
@@ -1,7 +1,14 @@
 namespace EmaticsSwapiTest.Models
 {
-    public class Film
+    public record Film
     {
-
+        public string Title { get; set; }
+        public int Episode_Id { get; set; }
+        public string Opening_Crawl { get; set; }
+        public DateTime Release_Date { get; set; }
+        public string[] Characters { get; set; }
+        public string[] Planets { get; set; }
+        public string Url { get; set; }
     }
+
 }
--- EmaticsSwapiTest/SwapiClient.cs
+++ EmaticsSwapiTest/SwapiClient.cs
@@ -1,14 +1,32 @@
-﻿using EmaticsSwapiTest.ViewModels;
+﻿using EmaticsSwapiTest.Models;
+using EmaticsSwapiTest.ViewModels;
+using Microsoft.AspNetCore.Http.Json;
+using System.Text.Json;
 
 namespace EmaticsSwapiTest
 {
     public class SWApiClient(HttpClient client) : ISWApiClient
     {
         private HttpClient Client { get; } = client;
+        private JsonSerializerOptions JsonOptions { get; } = new() { PropertyNameCaseInsensitive = true };
 
         public async Task<FilmsViewModel> GetFilms(string url)
         {
-            throw new NotImplementedException();
+            Film[] filmsData = await GetAllFilms(url);
+
+            FilmsViewModel model = new()
+            {
+                FilmsTable = [..
+                    filmsData.Select(f => new FilmInfo
+                    {
+                        Title = f.Title,
+                        EpisodeId = f.Episode_Id,
+                        ReleaseDate = f.Release_Date,
+                        FilmUrl = f.Url
+                    })
+                ]
+            };
+            return model;
         }
 
         public async Task<FilmDetailsViewModel?> GetFilmDetails(string url)
@@ -16,5 +34,36 @@ namespace EmaticsSwapiTest
             throw new NotImplementedException();
         }
 
+        private async Task<Film[]?> GetAllFilms(string url)
+        {
+            string responseBody = await SendRequest(url);
+            return JsonSerializer.Deserialize<Film[]?>(responseBody, JsonOptions);
+        }
+
+        private async Task<Film?> GetFilm(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<Character?> GetCharacter(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<Planet?> GetPlanet(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async Task<string> SendRequest(string url)
+        {
+            HttpRequestMessage request = new(HttpMethod.Get, url);
+
+            HttpResponseMessage response = await Client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
     }
 }
