b4de071ffc84c1c51c3dfe91fe4389dcf1ada9a8
Richard-A-Harris <richard.harris@timpson.com>
2025-02-13 19:00:38 +0000

    Add project files.

---
+++ EmaticsSwapiTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmaticsSwapiTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
